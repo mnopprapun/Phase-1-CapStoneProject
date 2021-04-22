c1e0aeb644344ef68f9673a083fd2f62a92ba6dc
Marc Nopprapun <44753875+mnopprapun@users.noreply.github.com>
2021-04-22 11:35:08 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 const test = "https://api.openweathermap.org/data/2.5/onecall?lat=29.6630&lon=-95.235489&exclude=minutely,hourly&units=imperial&appid=51e464ca4cdfb256e268caccccef8edf"
 var today = new Date();
-var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate());
+var date = today.toLocaleString('en-us', { weekday: 'short' });
 
 fetch(test)
   .then(res => res.json())
@@ -10,10 +10,12 @@ fetch(test)
     fetchTemp(data)
   })
 
-let currentTemp 
+let currentTemp
+
+// this sections put the current temps and todays highs and lows
 
 function fetchTemp(tempData) {
-  
+
   currentTemp = tempData
 
   let selDate = document.querySelector('.day')
@@ -22,72 +24,110 @@ function fetchTemp(tempData) {
   let selHumidity = document.querySelector('.humidity')
   selHumidity.innerText = tempData.current.humidity + ' % Humidity Levels'
 
-  let selDescription = document.querySelector('.description')
-  selDescription.innerText = tempData.current.weather[0].description
+  let selDes = document.querySelector('.description')
+  selDes.innerText = tempData.current.weather[0].description
 
   let selTemp = document.querySelector('.temp')
-  selTemp.innerHTML = "Current Temp " + tempData.current.temp + '&deg;F'
+  selTemp.innerHTML = "Current " + tempData.current.temp + '&deg;F'
+
+  document.querySelector('.highs').innerHTML = "Highs " + tempData.daily[0].temp.max + '&deg;F'
+
+  document.querySelector('.lows').innerHTML = "Lows " + tempData.daily[0].temp.min + '&deg;F'
+
+ //comment and submit
+
+  const commentList = document.querySelector('.comments')
+
+  document.querySelector('.comment-form').addEventListener('submit', event => {
+    event.preventDefault()
+    const addComment = document.createElement('li')
+    console.log(addComment)
+    commentList.appendChild(addComment)
+    const newCommentInput = document.querySelector('.comment-input')
+    addComment.innerText = newCommentInput.value
+    event.target.reset()
+
+    let newCommentData = {
+      "imageId": 1,
+      "content": addComment.innerText
+    }
+    postComments(newCommentData)
+
+    function postComments(newComment) {
+      let optionsPost = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(newComment)
+
+      }
+      fetch("http://localhost:3000/comments", optionsPost)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+  })
+
+  // delete comment button
+
+  let deleteButton = document.querySelector('.delete-button')
+  deleteButton.addEventListener('click', function (event) {
+    event.preventDefault('click', () => {
+      let options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }
+      }
+      fetch("http://localhost:3000/comments", options)
+        .then(response => response.json())
+        .then(comments => comments.remove())
+    })
+  })
+
+// submit forecast button
 
   let submitBtn = document.querySelector('.submitbtn')
   submitBtn.addEventListener('click', function (event) {
     event.preventDefault()
-    // POST forecast
-    fetch("http://localhost:3000/forecasts", {
-      method: 'POST',
-      body:JSON.stringify(tempData),
-      headers: {
-        "Content-type": "application/json"
-      }
-    })
-    .then(res=>res.json())
-    .then(json =>console.log(json))
-  })
+
+  }
+  )
+
+// console.log information for live feed
 
   //tomorrow forecast
   console.log(tempData.daily[0].temp.max)
   console.log(tempData.daily[0].temp.min)
   console.log(tempData.daily[0].weather[0].description)
-// tomorrow + 1
+  // tomorrow + 1
   console.log(tempData.daily[1].temp.max)
   console.log(tempData.daily[1].temp.min)
   console.log(tempData.daily[1].weather[0].description)
-// tomorrow + 2
+  // tomorrow + 2
   console.log(tempData.daily[2].temp.max)
   console.log(tempData.daily[2].temp.min)
   console.log(tempData.daily[2].weather[0].description)
-// tomorrow + 3
+  // tomorrow + 3
   console.log(tempData.daily[3].temp.max)
   console.log(tempData.daily[3].temp.min)
   console.log(tempData.daily[3].weather[0].description)
-// tomorrow + 4
+  // tomorrow + 4
   console.log(tempData.daily[4].temp.max)
   console.log(tempData.daily[4].temp.min)
   console.log(tempData.daily[4].weather[0].description)
-// tomorrow + 5
+  // tomorrow + 5
   console.log(tempData.daily[5].temp.max)
   console.log(tempData.daily[5].temp.min)
   console.log(tempData.daily[5].weather[0].description)
-// tomorrow + 6
+  // tomorrow + 6
   console.log(tempData.daily[6].temp.max)
   console.log(tempData.daily[6].temp.min)
   console.log(tempData.daily[6].weather[0].description)
-// tomorrow + 7
+  // tomorrow + 7
   console.log(tempData.daily[7].temp.max)
   console.log(tempData.daily[7].temp.min)
   console.log(tempData.daily[7].weather[0].description)
 }
-
-let deleteButton = document.querySelector('.delete-button')
-deleteButton.addEventListener('click', function (event) {
-  event.preventDefault()
-  //Backend delete
-  let options = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    }
-  };
-})
-
-///checking
